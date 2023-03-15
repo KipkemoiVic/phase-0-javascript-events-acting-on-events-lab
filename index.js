21132376f9b54c55f5d13076c1575805aeec6a87
VICTOR MITINGI <mitingi.victor@gmail.com>
2023-03-15 16:15:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    const currentLeft = parseInt(dodger.style.left);
+    if (currentLeft < 360) {
+      dodger.style.left = `${currentLeft + 4}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+  function moveDodgerLeft() {
+    var leftNumbers = dodger.style.left.replace("px", "");
+    var left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 4}px`;
+    }
+  }
+  
+
+
